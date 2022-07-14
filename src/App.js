ed575e1994d9c0520cf15dfd55e8c42bac82b752
Edgar Ruiz <edgarrruizl@gmail.com>
2022-07-14 12:11:37 +0100

    added back to all repositories button

--- src/App.js
+++ src/App.js
@@ -8,23 +8,12 @@ import { IndexPage } from './pages';
 
 
 function App() {
-  // const profileData = useSelector((state) => state.profileData);
-  // const show = useSelector((state) => state.showProfile);
-  // const repoData = useSelector((state) => state.repoData);
 
-  
   return (
     <>
       <Navbar />
       <IndexPage />
-      {/* {!show && <UsernameForm />}
-      {show && (
-        <UserCard
-          userName={profileData.login}
-          imageURL={profileData.avatar_url}
-          repoList={profileData.repoData}
-        />
-      )} */}
+ 
     </>
   );
 }
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,12 +1,38 @@
-import RepoItem from "../RepoItem";
+import RepoItem from '../RepoItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { reposDataSetter, setError404 } from '../../actions';
+import axios from 'axios';
 
 export default function ReposList(props) {
+  const dispatch = useDispatch();
+  const isSingleRepo = useSelector((state) => state.isSingleRepo);
+
+  const setUserData = async (query) => {
+    try {
+      const result = await axios.get(
+        `https://api.github.com/users/${query}/repos`
+      );
+      dispatch(reposDataSetter(result.data));
+    } catch (error) {
+      dispatch(setError404());
+      console.warn(error.message);
+    }
+  };
+
+  function repositoriesButton() {
+    setUserData(props.data[0].owner.login);
+  }
+
   return (
     <div>
       <h1>{`${props.data[0].owner.login}' Repos`}</h1>
+      <h3>Click on a repository name to get more details!</h3>
       {props.data.map((repo) => (
         <RepoItem data={repo} key={repo.id} />
       ))}
+      {isSingleRepo && (
+        <button onClick={repositoriesButton}>Back to all repositories</button>
+      )}
     </div>
   );
 }
